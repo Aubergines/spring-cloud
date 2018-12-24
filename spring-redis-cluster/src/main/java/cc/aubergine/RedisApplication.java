dba5f5ba51b583fa4baf97c31171d1eb4eb45e0b
zhaoshuangquan <zhaoshuangquan@allinmd.cn>
2018-12-24 15:30:06 +0800

    redis cluster

---
+++ spring-redis-cluster/src/main/java/cc/aubergine/RedisApplication.java
@@ -0,0 +1,16 @@
+package cc.aubergine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class RedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisApplication.class, args);
+	}
+
+}
+
