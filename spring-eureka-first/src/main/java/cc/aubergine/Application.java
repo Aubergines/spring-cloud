4734ae20925b00b60893b938537282a26f14434e
zhaoshuangquan <zhaoshuangquan@allinmd.cn>
2019-01-02 00:29:50 +0800

    maven 镜像仓库修改

--- spring-eureka-first/src/main/java/cc/aubergine/Application.java
+++ spring-eureka-first/src/main/java/cc/aubergine/Application.java
@@ -3,8 +3,9 @@ package cc.aubergine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableDiscoveryClient
+@EnableEurekaServer
 @SpringBootApplication
 public class Application {
 
--- spring-redis-cluster/src/main/java/cc/aubergine/RedisApplication.java
+++ spring-redis-cluster/src/main/java/cc/aubergine/RedisApplication.java
@@ -3,8 +3,10 @@ package cc.aubergine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
-@EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
 public class RedisApplication {
 
